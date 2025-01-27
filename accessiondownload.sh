99d540bf067482c6d8814216d1ea32fe703cd13d
William Schmitt <schmittw@wustl.edu>
2025-01-27 16:52:57 -0600

    fixed bash script issue

--- accessiondownload.sh
+++ accessiondownload.sh
@@ -3,7 +3,9 @@ ACCESSION_FILE="accession_numbers.txt"
 # Base URL for downloading files from PDB
 BASE_URL="https://files.rcsb.org/download"
 # Read accession numbers from the file and download each file
-while IFS= read -r ACCESSION; do
+# Read the file into an array
+mapfile -t ACCESSION_ARRAY < "$ACCESSION_FILE"
+for ACCESSION in "${ACCESSION_ARRAY[@]}"; do
   ACCESSION=$(echo "$ACCESSION" | tr -d '\r')
   echo "Downloading file for accession number: $ACCESSION"
   echo "$ACCESSION.pdb"
